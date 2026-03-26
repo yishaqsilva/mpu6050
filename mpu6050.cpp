ac8a9b01e2d6fed4e21f50e4cc0bd9377ecbcdad
yishaqsilva <yishaqsilva@gmail.com>
2026-03-26 16:19:17 +0100

    feat: raw values, lsb

--- mpu6050.cpp
+++ mpu6050.cpp
@@ -8,6 +8,7 @@
 #include <stdio.h>
 
 mpu6050::mpu6050(int n) {
+
     char bus[11];
     snprintf(bus, sizeof(bus), "/dev/i2c-%d", n);
     fd = open(bus, O_RDWR);
@@ -32,11 +33,11 @@ __u8 mpu6050::read_byte(__u8 reg) {
     return response;
 }
 
-int mpu6050::write_byte(__u8 reg, __u8 byte){
+void mpu6050::write_byte(__u8 reg, __u8 byte){
 
     __u8 msg[2] = {reg, byte};
 
-    return write(fd, msg, sizeof(msg));
+    write(fd, msg, sizeof(msg));
 }
 
 __u16 mpu6050::read_word(__u8 regA, __u8 regB){
@@ -55,44 +56,97 @@ void mpu6050::wake_up(){
     write_byte(0x6B, 0x00);
 }
 
-__u16 mpu6050::gyro_x(){
+__u16 mpu6050::gyro_x_raw(){
     return read_word(0x43, 0x44);   
 }
-__u16 mpu6050::gyro_y(){
+__u16 mpu6050::gyro_y_raw(){
     return read_word(0x45, 0x46);   
 }
-__u16 mpu6050::gyro_z(){
+__u16 mpu6050::gyro_z_raw(){
     return read_word(0x47, 0x48);   
 }
 
-__u16 mpu6050::accel_x(){
+float mpu6050::gyro_x(){
+    return read_word(0x43, 0x44) / gyro_lsb;
+}
+float mpu6050::gyro_y(){
+    return read_word(0x45, 0x46) / gyro_lsb;
+}
+float mpu6050::gyro_z(){
+    return read_word(0x47, 0x48) / gyro_lsb;
+}
+
+__u16 mpu6050::accel_x_raw(){
     return read_word(0x3b, 0x3c);
 }
-__u16 mpu6050::accel_y(){
+__u16 mpu6050::accel_y_raw(){
     return read_word(0x3d, 0x3e);
 }
-__u16 mpu6050::accel_z(){
+__u16 mpu6050::accel_z_raw(){
     return read_word(0x3f, 0x40);
 }
 
-__u16 mpu6050::temp(){
-    return read_word(0x41, 0x42);
+float mpu6050::accel_x(){
+    return read_word(0x3b, 0x3c) / accl_lsb;
+}
+float mpu6050::accel_y(){
+    return read_word(0x3d, 0x3e) / accl_lsb;
+}
+
+float mpu6050::accel_z(){
+    return read_word(0x3f, 0x40) / accl_lsb;
+
+float mpu6050::temp(){
+    return (read_word(0x41, 0x42) / 340.0f) + 36.53f;
 }
 
 __u8 mpu6050::ext_sens_data(__u8 n){
     return read_byte(0x49 + n);
 }
 
-void mpu6050::config(__u8 value){
-    write_byte(0x1A, 0x00);
-    write_byte(0x1A, value);
+void mpu6050::accel_config_afs_sel(int n) {
+    
+    __u8 conf = read_byte(0x1C);
+
+    //preserve XA_ST, YA_ST, ZA_ST bits
+    conf = prev & 0xE0;
+
+    switch(n) {
+        case 0: 
+            write_byte(0x1C, conf);
+            accl_lsb = 16384.0f;
+        case 1:
+            write_byte(0x1C, conf | 0x08);
+            accl_lsb = 8192.0f;
+        case 2:
+            write_byte(0x1C, conf | 0x10);
+            accl_lsb = 4096.0f;
+        case 3:
+            write_byte(0x1C, conf | 0x18);
+            accl_lsb = 2048.0f;
+        default:
+            perror("max value is 3 - input is %d, n");
+    }
 }
 
-void mpu6050::accel_config(__u8 value){
-    write_byte(0x1C, 0x00);
-    write_byte(0x1C, value);
-}
-void mpu6050::gyro_config(__u8 value){
-    write_byte(0x1B, 0x00);
-    write_byte(0x1B, value);
+void mpu6050::gyro_config_fs_sel(int n){
+
+    __u8 conf = 0x00;
+
+    switch(n) {
+        case 0: 
+            write_byte(0x1C, conf);
+            gyro_lsb = 131.0f;
+        case 1:
+            write_byte(0x1C, conf | 0x08);
+            gyro_lsb = 65.5f;
+        case 2:
+            write_byte(0x1C, conf | 0x10);
+            gyro_lsb = 32.8f;
+        case 3:
+            write_byte(0x1C, conf | 0x18);
+            gyro_lsb = 16.4f;
+        default:
+            perror("max value is 3 - input is %d, n");
+    }
 }
--- mpu6050.h
+++ mpu6050.h
@@ -2,39 +2,47 @@
 #define MPU6050_H
 #include <linux/types.h>
 #define ADDR 0x68
-#define EXT_SYNC_SET 0x38 //config
-#define DLPF_CFG 0x08
-#define AFS_SEL 0x18 //accel config
 #define ZA_ST 0x20
 #define YA_ST 0x40
 #define XA_ST 0x80
-#define FS_SEL 0x18 //gyro config
 
 class mpu6050 {
 
     private:
         int fd;
+        float accl_lsb = 16384.0f;
+        float gyro_lsb = 131.0f;
 
     public:
        mpu6050(int bus); 
        __u8 read_byte(__u8 reg); //read from reg
-       int write_byte(__u8 reg, __u8 byte); //write value to reg
+        void write_byte(__u8 reg, __u8 byte); //write value to reg
        __u16 read_word(__u8 regA, __u8 regB);
 
        __u8 who_am_i();
        void wake_up();
-       __u16 gyro_x();
-       __u16 gyro_y();
-       __u16 gyro_z();
-       __u16 accel_x();
-       __u16 accel_y();
-       __u16 accel_z();
-       __u16 temp();
+
+       __u16 gyro_x_raw();
+       __u16 gyro_y_raw();
+       __u16 gyro_z_raw();
+
+       float gyro_x();
+       float gyro_y();
+       float gyro_z();
+
+       __u16 accel_x_raw();
+       __u16 accel_y_raw();
+       __u16 accel_z_raw();
+
+       float accel_x();
+       float accel_y();
+       float accel_z();
+       float temp();
+
        __u8 ext_sens_data(__u8 n);
      
-       void config(__u8 value);
-       void accel_config(__u8 value);
-       void gyro_config(__u8 value);
+       void accel_config_afs_sel(int n);
+       void gyro_config_fs_sel(int n);
 };  
 
 #endif
